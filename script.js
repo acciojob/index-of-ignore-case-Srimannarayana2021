c5878773827633b686ec93f7d8e2dc211bb21e1a
Srimannarayana <87487895+Srimannarayana2021@users.noreply.github.com>
2025-08-30 10:22:28 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,5 +1,12 @@
 function indexOfIgnoreCase(s1, s2) {
   // write your code here
+	s2=s2.toLowerCase();
+	s1=s1.toLowerCase();
+	for(let i=0;i<=s1.length-s2.length;i++){
+		const l2 = s2.length;
+		if(s2 === s1.slice(i,i+l2)) return i;
+	}
+	return -1;
 }
 
 // Please do not change the code below
